ea7888e2c93c5aab7c329077173b954cae68e09e
Alanna Paiva <alannapaiva1@hotmail.com>
2021-08-03 22:56:44 -0300

    create the first card game

---
+++ main.js
@@ -0,0 +1,12 @@
+import CardGame from './src/components/CardGame';
+import './src/styles/settings/colors.css';
+import './src/styles/elements/base.css';
+
+const $root = document.querySelector("#root");
+const $htmlCardGame = CardGame()
+
+
+
+$root.insertAdjacentHTML("beforeend", $htmlCardGame); 
+
+
---
+++ src/components/CardGame/index.js
@@ -0,0 +1,10 @@
+import "./style.css";
+
+function CardGame (){
+    return `
+    <article class ="card-game">
+        <img src = "images/alura-pixel 1.svg" alt ="Logo da Alura">
+    </article> 
+    `;
+}
+export default CardGame;
