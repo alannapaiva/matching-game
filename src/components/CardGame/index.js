b4233541f335daed910aa06429e48ee383c6a751
Alanna Paiva <alannapaiva1@hotmail.com>
2021-08-09 22:16:51 -0300

    Create BoardGame object without CSS

--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -2,9 +2,12 @@ import "./style.css";
 
 function CardGame (){
     return `
+    
     <article class ="card-game">
-        <img src = "images/alura-pixel 1.svg" alt ="Logo da Alura">
+        <img class = "img-card" src = "images/alura-pixel 1.svg" alt ="Logo da Alura">
     </article> 
+  
+       
     `;
 }
 export default CardGame;
---
+++ src/objects/BoardGame/index.js
@@ -0,0 +1,12 @@
+import CardGame from "../../components/cardGame";
+
+function BoardGame(amountCards) {
+    const $htmlCardGame = CardGame();
+    const $htmlBoardGame = $htmlCardGame.repeat(amountCards);
+
+
+    return $htmlBoardGame;
+}
+
+
+export default BoardGame;
